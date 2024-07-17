ad99df7bf712f7234787918c2478b6d7c18e7ea6
larbi <99117566+larbi1512@users.noreply.github.com>
2024-07-17 23:49:32 +0100

    about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import HeroSections from "./components/HeroSections";
+import SkillHero from "./components/SkillHero";
+import About from "./components/About"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col mx-auto lg:px-[80px] bg-main">
+    <main className="  max-h-screen flex-col bg-main ">
       <HeroSections/>
+      <SkillHero/>
+      <About/>
+
     </main>
   );
 }
